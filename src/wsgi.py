4d49f828974739f8eb4134951e7fef7c9c887639
Elpala <luis.pala1997@gmail.com>
2020-06-17 14:09:28 -0500

    cambio en procfile

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,9 +1,9 @@
-from app import dash as application
+from app.dash import app as application
 from config import config
 
 
 def main():
-    application.app.run_server(debug=config.debug, host=config.host, port=config.port)
+    application.run_server(debug=config.debug, host=config.host, port=config.port)
 
 
 if __name__ == "__main__":
