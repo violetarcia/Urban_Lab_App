e1f1a8e7694d4a44819e0e18d2439c1cf7a9e0e9
Francisco ME <if.francisco.me@gmail.com>
2020-06-20 13:27:24 -0500

    Modificaciones de formato

--- src/app/dash.py
+++ src/app/dash.py
@@ -25,7 +25,6 @@ from model.visualizaciones import map_metric
 import model.entradas as ent
 
 
-
 # - - - - - - - - - - -
 
 data = Data()
--- src/model/visualizaciones.py
+++ src/model/visualizaciones.py
@@ -1,4 +1,3 @@
-
 # .. ................................................................................... .. #
 # .. Proyecto: SocialSim - Plataforma de simulacion de proyectos socioproductivos        .. #
 # .. Archivo: visualizaciones.py - diccionario con datos de entrada                      .. #
@@ -8,14 +7,16 @@
 # .. ................................................................................... .. #
 
 # Importing and initializing main Python libraries
-import datos as dat
+import models.datos as dat
 import plotly.express as px
+
 
 # -- ------------------------------------------------------------------------------------ -- #
 # -- Function: Map
 # -- ------------------------------------------------------------------------------------ -- #
+
 def map_metric(df_data, metric, path_shape, path_kml):
-	"""
+    """
     Parameters
     ---------
     :param:
@@ -34,18 +35,15 @@ def map_metric(df_data, metric, path_shape, path_kml):
 		metric = 'estres'
 		path: str : "cp_jal_2/CP_14_Jal_v6.shp"
 	"""
-	j_file = dat.read_map_files(path_shape, path_kml)
-	fig = px.choropleth_mapbox(df_data, geojson=j_file, locations= 'CP', color=metric,
-	                           color_continuous_scale="Viridis",
-	                           range_color=(0, max(df_data[metric])),
-	                           mapbox_style="carto-positron",
-	                           zoom=10, center = {"lat": 20.666820, "lon": -103.3918228},
-	                           opacity=0.5,
-	                           labels={metric:'Nivel de ' + metric}
-	                          )
-	fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-	return fig
+    j_file = dat.read_map_files(path_shape, path_kml)
+    fig = px.choropleth_mapbox(df_data, geojson=j_file, locations='CP', color=metric,
+                               color_continuous_scale="Viridis",
+                               range_color=(0, max(df_data[metric])),
+                               mapbox_style="carto-positron",
+                               zoom=10, center={"lat": 20.666820, "lon": -103.3918228},
+                               opacity=0.5,
+                               labels={metric: 'Nivel de ' + metric}
+                               )
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
-
-
-
+    return fig
