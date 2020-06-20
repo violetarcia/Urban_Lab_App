65c286f2bdaa567548fdaf8e4e8427ce51496ba3
Francisco ME <if.francisco.me@gmail.com>
2020-06-20 09:49:56 -0500

    Modificaciones de formato

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,3 +1,12 @@
+
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: Urban Lab / Laboratorio de econometría espacial urbana - PAP4J05                           -- #
+# -- script: wsgi.py : script para inicializar el dashboard                                              -- #
+# -- author: FranciscoME / PAP4J05                                                                       -- #
+# -- license: MIT                                                                                        -- #
+# -- repository: https://github.com/IFFranciscoME/Urban_Lab_App                                          -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+
 from app.dash import app
 from config import config
 server = app.server
