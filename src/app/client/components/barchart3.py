3593e5b7cefef222acafa061dedcf67ac3c38fa8
elpala <luis.pala1997@gmail.com>
2020-06-29 14:46:15 -0500

    update, agregando tablas

--- src/app/client/components/barchart3.py
+++ src/app/client/components/barchart3.py
@@ -3,5 +3,18 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 from app.dash import app
+from model.visualizaciones import table_giro
+from model.datos import dict_metrics_table, df_pymes
 
 barchart3 = dcc.Graph(id='barchart3', figure={})
+@app.callback(
+    Output(component_id='barchart3', component_property='figure'),
+    [Input(component_id='slct_map', component_property='value')]
+)
+def update_graph(option_map):
+
+    # Visualizations
+    fig = table_giro(df_pymes, option_map, dict_metrics_table[option_map])
+    
+    return fig
+
