59c2aaf61750be74079d7f73dbbaf7cd857c95a5
elpala <luis.pala1997@gmail.com>
2020-07-10 22:51:02 -0500

    pruebas de mejora de footer

--- src/app/client/utils/footer.py
+++ src/app/client/utils/footer.py
@@ -3,110 +3,106 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from app.dash import app
 
-footer = html.Div(
-    [
+footer = html.Footer(
+    children=[
         html.Div(
             [
                 html.Div(
                     [
-                        html.Img(
-                            src=app.get_asset_url('logo2.png'),
-                            className='w-100 m-auto rounded'
-                        )
-                    ],
-                    className='col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4'),
-                html.Hr(
-                    className='clearfix w-100 d-md-none bg-white'),
-                html.Div(
-                    [
-                        html.P(
-                            children=[
-                                'Este proyecto fue elaborado por estudiantes del PAP 4J05 de la universidad ITESO, durante el periodo de Verano 2020. Para consultar detalles dirigirse a: ',
-                                html.A(
-                                    children=[
-                                        html.I(
-                                            className='fab fa-github'),
-                                        ' Code'
-                                    ],
-                                    href='https://github.com/IFFranciscoME/Urban_Lab_App/',
-                                    target='_blank',
-                                    className='text-white'
-                                )
-                            ],
-                            className='text-center text-justify m-auto'
-                        )
-                    ],
-                    className='col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 d-flex'),
-                html.Hr(
-                    className='clearfix w-100 d-md-none bg-white'),
-                html.Div(
-                    [
                         html.Div([
-                            html.H5(
-                                'Referencias',
-                                className='font-weight-bold text-warning'
-                            ),
-                            html.Ul(
+                            html.Img(
+                                src=app.get_asset_url('logo2.png'),
+                                className='w-100 m-auto rounded'
+                            )
+                        ], className='col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 mt-3'),
+                        html.Hr(className='clearfix w-100 d-md-none bg-white'),
+                        html.Div([
+                            html.P(
                                 children=[
-                                    html.Li(
+                                    'Este proyecto fue elaborado por estudiantes del PAP 4J05 de la universidad ITESO, durante el periodo de Verano 2020. Para consultar detalles dirigirse a: ',
+                                    html.A(
                                         children=[
-                                            html.A(
-                                                children=[
-                                                    html.I(
-                                                        className='fa fa-paperclip'),
-                                                    '  Github'
-                                                ],
-                                                href='https://github.com/IFFranciscoME/Urban_Lab_App/',
-                                                target='_blank',
-                                                className='text-white'
-                                            )
-                                        ]
-                                    ),
-                                    html.Li(
-                                        children=[
-                                            html.A(
-                                                children=[
-                                                    html.I(
-                                                        className='fa fa-paperclip'),
-                                                    '  Notas Tecnicas'
-                                                ],
-                                                href='https://github.com/IFFranciscoME/Urban_Lab_App/',
-                                                target='_blank',
-                                                className='text-white'
-                                            )
-                                        ]
-                                    ),
-                                    html.Li(
-                                        children=[
-                                            html.A(
-                                                children=[
-                                                    html.I(
-                                                        className='fa fa-paperclip'),
-                                                    '  Datos'
-                                                ],
-                                                href='https://github.com/IFFranciscoME/Urban_Lab_App/',
-                                                target='_blank',
-                                                className='text-white'
-                                            )
-                                        ]
+                                            html.I(
+                                                className='fab fa-github'),
+                                            ' Code'
+                                        ],
+                                        href='https://github.com/IFFranciscoME/Urban_Lab_App/',
+                                        target='_blank',
+                                        className='text-white'
                                     )
                                 ],
-                                className='list-unstyled'
+                                className='text-center text-justify m-auto'
                             )
-                        ],
-                        className='m-auto')
+                        ], className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 d-flex mt-3'),
+                        html.Hr(className='clearfix w-100 d-md-none bg-white'),
+                        html.Div([
+                            html.Div([
+                                html.H5(
+                                    'Referencias',
+                                    className='font-weight-bold text-warning'
+                                ),
+                                html.Ul(
+                                    children=[
+                                        html.Li(
+                                            children=[
+                                                html.A(
+                                                    children=[
+                                                        html.I(
+                                                            className='fa fa-paperclip'),
+                                                        '  Github'
+                                                    ],
+                                                    href='https://github.com/IFFranciscoME/Urban_Lab_App/',
+                                                    target='_blank',
+                                                    className='text-white'
+                                                )
+                                            ]
+                                        ),
+                                        html.Li(
+                                            children=[
+                                                html.A(
+                                                    children=[
+                                                        html.I(
+                                                            className='fa fa-paperclip'),
+                                                        '  Notas Tecnicas'
+                                                    ],
+                                                    href='https://github.com/IFFranciscoME/Urban_Lab_App/',
+                                                    target='_blank',
+                                                    className='text-white'
+                                                )
+                                            ]
+                                        ),
+                                        html.Li(
+                                            children=[
+                                                html.A(
+                                                    children=[
+                                                        html.I(
+                                                            className='fa fa-paperclip'),
+                                                        '  Datos'
+                                                    ],
+                                                    href='https://github.com/IFFranciscoME/Urban_Lab_App/',
+                                                    target='_blank',
+                                                    className='text-white'
+                                                )
+                                            ]
+                                        )
+                                    ],
+                                    className='list-unstyled'
+                                )
+                            ],
+                                className='m-auto')
+                        ], className='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2  mt-3')
                     ],
-                    className='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2')
+                    className='row mb-3 mt-3'),
+                html.Hr(className='bg-white p-0 m-0'),
+                html.P(
+                    children=[
+                        'Copyright @2020 | Diseñado por Laboratorio de econometría espacial urbana'
+                    ],
+                    className='mt-2'
+                )
             ],
-            className='row mb-3 mt-3'
-        ),
-        html.Hr(className='bg-white p-0 m-0'),
-        html.P(
-            children=[
-                'Copyright @2020 | Diseñado por Laboratorio de econometría espacial urbana'
-            ],
-            className='mt-2'
+            className='container text-center text-white'
         )
     ],
-    className='container-fluid mt-auto bg-dark text-center text-white '
+    className='w-100 bg-dark mt-auto'
 )
